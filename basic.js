05e07de2dccf843c05be369fe2c627d3d760bc54
Anant-1 <54532485+Anant-1@users.noreply.github.com>
2023-11-26 03:20:56 +0530

    Add map, Reduce, filter, and arrow func

--- basic.js
+++ basic.js
@@ -31,8 +31,8 @@
 
 // const [fun,sum] = (n1, n2) => (n1+n2, {username:'anant'})
 // const [sum, obj] = (n1, n2) => [(n1 + n2), { username: 'anant' }];
-const [sum, obj] = ((n1, n2) => [(n1 + n2), { username: 'anant' }])(5, 7);
-console.log(sum, obj);
+// const [sum, obj] = ((n1, n2) => [(n1 + n2), { username: 'anant' }])(5, 7);
+// console.log(sum, obj);
 
 // console.log(sum(1, 3), obj())
 
@@ -45,3 +45,31 @@ console.log(sum, obj);
 //     console.log('unnamed arrow iife')
 //     console.log(n1, n2)
 // })()
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const filteredNums = myNums.filter( (num)=>{
+//     return num>4;
+// })
+// console.log(filteredNums)
+
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if(num>4) newNums.push(num)
+// })
+// console.log(newNums)
+
+// const newNums = myNums.map((num)=>num+10)
+// console.log(newNums)
+
+// const newNums = myNums.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>20)
+// console.log(newNums)
+
+// const myTotal = myNums.reduce(function(acc, currVal){
+//     console.log(`acc: ${acc} and currVal: ${currVal}`)
+//     return acc + currVal
+// }, 0)
+// console.log(myTotal)
+
+const myTotal = myNums.reduce((acc, currVal)=>(acc + currVal), 0)
+console.log(myTotal)
